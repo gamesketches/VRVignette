a7cf5d9053e16a4b1b5e23fc738eb6e9809dc1ae
gamesketches <sam.von.ehren@gmail.com>
2016-03-26 13:58:56 -0400

    Made a child model which is faded out. Need to code the reveal though

---
+++ Assets/CyclopsLaser.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CyclopsLaser : MonoBehaviour {
+
+	public float rayLength;
+	GameObject lastLookedAt;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray PsychicRay = new Ray(transform.position, transform.forward);
+
+		if(Physics.Raycast(PsychicRay, out hit, rayLength)){
+			hit.collider.gameObject.GetComponent<RevealGhost>().increaseAlpha();
+		}
+		Debug.DrawRay(transform.position, transform.forward * rayLength);
+	}
+}
---
+++ Assets/RevealGhost.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevealGhost : MonoBehaviour {
+
+	public GameObject ghost;
+	private Renderer[] ghostRenderers;
+	private float alphaBlend;
+	// Use this for initialization
+	void Start () {
+		ghostRenderers = ghost.GetComponentsInChildren<Renderer>();
+		for(int i = 0; i < ghostRenderers.Length; i++) {
+			ghostRenderers[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		alphaBlend -= 0.01f;
+		for(int i = 0; i < ghostRenderers.Length; i++) {
+			Renderer renderer = ghostRenderers[i];
+			for(int k = 0; k < renderer.materials.Length; k++) {
+				Color materialColor = renderer.materials[k].color;
+				materialColor.a = alphaBlend;
+				renderer.materials[k].color = materialColor;
+			}
+		}
+	}
+
+	public void increaseAlpha() {
+		alphaBlend += 0.02f;
+	}
+
+}
