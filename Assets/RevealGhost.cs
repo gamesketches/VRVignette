01191b94f70e089ac8e783eff91db6b576402a87
gamesketches <sam.von.ehren@gmail.com>
2016-03-26 14:05:27 -0400

    Some more tuning on the alpha blending, doesn't work exactly how I want yet but getting warmer

--- Assets/RevealGhost.cs
+++ Assets/RevealGhost.cs
@@ -16,7 +16,9 @@ public class RevealGhost : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log(alphaBlend);
 		alphaBlend -= 0.01f;
+		alphaBlend = alphaBlend < 0f ? 0f : alphaBlend;
 		for(int i = 0; i < ghostRenderers.Length; i++) {
 			Renderer renderer = ghostRenderers[i];
 			for(int k = 0; k < renderer.materials.Length; k++) {
@@ -28,7 +30,9 @@ public class RevealGhost : MonoBehaviour {
 	}
 
 	public void increaseAlpha() {
-		alphaBlend += 0.02f;
+		alphaBlend += 0.1f;
+		alphaBlend = alphaBlend > 1.0f ? 1f : alphaBlend;
+		Debug.Log("Increase Alpha");
 	}
 
 }
