f8fc33a35f0f6e2f238c85a9397acd5fa3c28946
gamesketches <sam.von.ehren@gmail.com>
2016-03-27 22:48:04 -0400

    Got basic audio stuff going

---
+++ Assets/SceneDirector.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneDirector : MonoBehaviour {
+
+	public GameObject dude;
+	public GameObject chick;
+	public Animator dudeGhost;
+	public Animator chickGhost;
+
+	private int conversationPoint;
+	private bool isTalking;
+	private AudioSource[] audioClips;
+	// Use this for initialization
+	void Start () {
+		AudioSource[] MarySources = chick.GetComponents<AudioSource>();
+		AudioSource[] JohnSources = dude.GetComponents<AudioSource>();
+		ShuffleArrays(JohnSources, MarySources);
+		isTalking = false;
+		conversationPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isTalking) {
+			StartCoroutine(PlayAudio());
+		}
+	}
+
+	void ShuffleArrays(AudioSource[] JohnSources, AudioSource[] MarySources) {
+		audioClips = new AudioSource[JohnSources.Length * 2];
+		int k = 0;
+		for(int i = 0; i < JohnSources.Length; i++) {
+			Debug.Log(k);
+			audioClips[k] = JohnSources[i];
+			k++;
+			Debug.Log(k);
+			audioClips[k] = MarySources[i];
+			k++;
+		}
+		for(int i = 0; i < audioClips.Length; i++) {
+			Debug.Log(audioClips[i].clip);
+		}
+	}
+
+	IEnumerator PlayAudio() {
+		isTalking = true;
+		if(conversationPoint < audioClips.Length) {
+			audioClips[conversationPoint].Play();
+			while(audioClips[conversationPoint].isPlaying) {
+				yield return null;
+			}
+
+			conversationPoint++;
+			isTalking = false;
+		}
+	}
+}
