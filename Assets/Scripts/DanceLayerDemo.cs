0576d3ba5870ddc79a60e18078d04d263e7b44a3
gamesketches <sam.von.ehren@gmail.com>
2016-05-26 13:22:44 -0400

    Added in navigational stuff

---
+++ Assets/Scripts/DanceLayerDemo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DanceLayerDemo : MonoBehaviour {
+
+	Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)) {
+			myAnimator.SetLayerWeight(1, 1.0f);
+
+		}
+		else {
+			myAnimator.SetLayerWeight(1, 0.0f);
+		}
+	}
+}
